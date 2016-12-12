d3954f232fe9292ed50aa9929722ac576b101c96
Flor <florenperetti@gmail.com>
2016-12-12 12:28:48 -0300

    se cachean las respuestas de la API (para el menu) y se agrega el contenido de la pagina antes del buscador.

--- actividad-publica-municipalidad-cordoba/actividad-publica-municipalidad-cordoba.php
+++ actividad-publica-municipalidad-cordoba/actividad-publica-municipalidad-cordoba.php
@@ -10,7 +10,8 @@ Author URI: https://github.com/florenperetti/
 
 add_action( 'plugins_loaded', array( 'ActividadesMuniCordoba', 'get_instancia' ) );
 
-class ActividadesMuniCordoba {
+class ActividadesMuniCordoba
+{
 	public static $instancia = null;
 	
 	private static $MESES = array("Ene", "Abr", "Ago", "Dic");
@@ -27,7 +28,8 @@ class ActividadesMuniCordoba {
 		return self::$instancia;
 	}
 	
-	private function __construct() {
+	private function __construct()
+	{
 		$this->plantillas = array();
 		// Agrega un filtro al metabox de atributos para inyectar la plantilla en el cache.
 			if ( version_compare( floatval( get_bloginfo( 'version' ) ), '4.7', '<' ) ) { // 4.6 y anteriores
@@ -63,12 +65,14 @@ class ActividadesMuniCordoba {
 		add_action( 'template_redirect', array($this, 'buscador_template_redirect') );
 	}
 	
-	public function agregar_plantilla_nueva( $posts_plantillas ) {
+	public function agregar_plantilla_nueva( $posts_plantillas )
+	{
 		$posts_plantillas = array_merge( $posts_plantillas, $this->plantillas );
 		return $posts_plantillas;
 	}
 	
-	public function registrar_plantillas( $atts ) {
+	public function registrar_plantillas( $atts )
+	{
 		// Crea la key usada para el cacheo de temas
 		$cache_key = 'page_templates-' . md5( get_theme_root() . '/' . get_stylesheet() );
 		// Recuperar la lista de caché. 
@@ -89,7 +93,8 @@ class ActividadesMuniCordoba {
 	/**
 	 * Se fija si la plantilla está asignada a la página
 	 */
-	public function ver_plantillas( $plantilla ) {
+	public function ver_plantillas( $plantilla )
+	{
 		global $post;
 		
 		if ( ! $post ) {
@@ -112,25 +117,64 @@ class ActividadesMuniCordoba {
 		}
 		
 		return $plantilla;
-	}	
+	}
 	
-	public function obtener_info() {
+	public function buscador_template_redirect()
+	{
+		if (is_page_template( 'buscador-actividades-template.php' )) {
+			$_POST["datos"] = $this->obtener_datos();
+			$_POST['URL_PLUGIN'] = plugins_url( '' , __FILE__);
+		}
+	}
+	
+	public function cargar_assets()
+	{
+		$urlCSSBuscador = plugins_url('/css/buscador.css', __FILE__);
+		$urlJSBuscador = plugins_url('/js/buscador.js',__FILE__ );
+		
+		wp_register_style('buscador_actividades.css', $urlCSSBuscador);
+		wp_register_script('buscador_actividades.js', $urlJSBuscador);
+
+		if (is_page_template('buscador-actividades-template.php')) {
+			
+			wp_enqueue_style( 'buscador_actividades.css', $urlCSSBuscador );
+			
+			wp_enqueue_script( 
+				'buscar_actividades_ajax', 
+				$urlJSBuscador, 
+				array('jquery'), 
+				'1.0.0',
+				TRUE 
+			);
+			
+			$nonce = wp_create_nonce( "buscar_actividad_nonce" );
+				
+			wp_localize_script( 
+				'buscar_actividades_ajax', 
+				'buscarActividad', 
+				array(
+					'url'   => admin_url( 'admin-ajax.php' ),
+					'nonce' => $nonce,
+				)
+			);
+		}
+	}
+	
+	public function obtener_datos()
+	{
 		$datos = [];
-	
-		$api_response = wp_remote_get( 'https://gobiernoabierto.cordoba.gob.ar/api/disciplina-actividad/' );
-		$datos['disciplinas'] = $this->chequear_respuesta($api_response, 'las disciplinas');
 		
-		$api_response = wp_remote_get( 'https://gobiernoabierto.cordoba.gob.ar/api/tipo-actividad/' );
-		$datos['tipo_actividad'] = $this->chequear_respuesta($api_response, 'los tipos de actividades');
+		// Se buscan los datos en cache, sino se llama a la api
 		
-		$api_response = wp_remote_get( 'https://gobiernoabierto.cordoba.gob.ar/api/agrupador-actividad/' );
-		$datos['eventos'] = $this->chequear_respuesta($api_response, 'los eventos');
+		$datos['disciplinas'] = $this->buscar_transient('disciplinas');
 		
-		$api_response = wp_remote_get( 'https://gobiernoabierto.cordoba.gob.ar/api/lugar-actividad/?audiencia_id=4' );
-		$datos['lugares'] = $this->chequear_respuesta($api_response, 'lugares');
+		$datos['tipo_actividad'] = $this->buscar_transient('tipo_actividad');
 		
-		$api_response = wp_remote_get( 'https://gobiernoabierto.cordoba.gob.ar/api/actividad-publica/' );
-		$datos['actividades'] = $this->chequear_respuesta($api_response, 'las actividades');
+		$datos['eventos'] = $this->buscar_transient('eventos');
+		
+		$datos['lugares'] = $this->buscar_transient('lugares');
+		
+		$datos['actividades'] = $this->buscar_transient('actividades');
 		
 		foreach($datos['actividades']['results'] as $key => $ac) {
 			$nombre = $ac['titulo'];
@@ -165,64 +209,58 @@ class ActividadesMuniCordoba {
 		return $datos;
 	}
 	
-	private function chequear_respuesta($api_response, $tipoObjeto) {
+	/*
+	* Mira si la respuesta es un error, si no lo es, cachea por una hora el resultado.
+	*/
+	private function chequear_respuesta($api_response, $tipoObjeto, $nombre_transient)
+	{
 		if (is_null($api_response)) {
 			return [ 'results' => [] ];
 		} else if (is_wp_error($api_response)) {
-			return [ 'results' => [], 'error' => 'Ocurrió un error al cargar '.$tipoObjeto.'.' ];
+			return [ 'results' => [], 'error' => 'Ocurri&oacute; un error al cargar '.$tipoObjeto.'.' ];
 		} else {
-			return json_decode( wp_remote_retrieve_body( $api_response ), true );
+			$respuesta = json_decode( wp_remote_retrieve_body( $api_response ), true );
+			set_transient('api_muni_cba_'.$nombre_transient, $respuesta, HOUR_IN_SECONDS );
+			return $respuesta;
 		}
 	}
 	
-	private function quitar_palabras($texto, $palabras_devueltas)
+	private function buscar_transient($nombre_transient)
 	{
-		$resultado = $texto;
-		$texto = preg_replace('/(?<=\S,)(?=\S)/', ' ', $texto);
-		$texto = str_replace("\n", " ", $texto);
-		$arreglo = explode(" ", $texto);
-		if (count($arreglo) <= $palabras_devueltas) {
-			$resultado = $texto;
+		$transient = get_transient('api_muni_cba_'.$nombre_transient);
+		$api_response = null;
+		$resultado = null;
+		if(!empty($transient)) {
+			return $transient;
 		} else {
-			array_splice($arreglo, $palabras_devueltas);
-			$resultado = implode(" ", $arreglo) . "...";
+			switch($nombre_transient) {
+				case 'disciplinas': {
+					$api_response = wp_remote_get( 'https://gobiernoabierto.cordoba.gob.ar/api/disciplina-actividad/' );
+					$resultado = $this->chequear_respuesta($api_response, 'las disciplinas', $nombre_transient);
+				} break;
+				case 'tipo_actividad': {
+					$api_response = wp_remote_get( 'https://gobiernoabierto.cordoba.gob.ar/api/tipo-actividad/' );
+					$resultado = $this->chequear_respuesta($api_response, 'los tipos de actividad', $nombre_transient);
+				} break;
+				case 'eventos': {
+					$api_response = wp_remote_get( 'https://gobiernoabierto.cordoba.gob.ar/api/agrupador-actividad/' );
+					$resultado = $this->chequear_respuesta($api_response, 'los eventos', $nombre_transient);
+				} break;
+				case 'lugares': {
+					$api_response = wp_remote_get( 'https://gobiernoabierto.cordoba.gob.ar/api/lugar-actividad/?audiencia_id=4' );
+					$resultado = $this->chequear_respuesta($api_response, 'los lugares', $nombre_transient);
+				} break;
+				case 'actividades': {
+					$api_response = wp_remote_get( 'https://gobiernoabierto.cordoba.gob.ar/api/actividad-publica/' );
+					$resultado = $this->chequear_respuesta($api_response, 'las actividades', $nombre_transient);
+				} break;
+			}
 		}
 		return $resultado;
 	}
 	
-	public function buscador_template_redirect()
+	public function buscar_actividad()
 	{
-		if (is_page_template( 'buscador-actividades-template.php' )) {
-			$_POST["datos"] = $this->obtener_info();
-			$_POST['URL_PLUGIN'] = plugins_url( '' , __FILE__);
-		}
-	}
-	
-	public function cargar_assets() {
-		wp_enqueue_style( 'buscador', plugins_url( '/css/buscador.css', __FILE__ ) );
-		
-		wp_enqueue_script( 
-			'buscar_actividades_ajax', 
-			plugins_url( '/js/buscador.js',__FILE__ ), 
-			array('jquery'), 
-			'1.0.0',
-			TRUE 
-		);
-		
-		$nonce = wp_create_nonce( "buscar_actividad_nonce" );
-			
-		wp_localize_script( 
-			'buscar_actividades_ajax', 
-			'buscarActividad', 
-			array(
-				'url'   => admin_url( 'admin-ajax.php' ),
-				'nonce' => $nonce,
-			)
-		);
-	}
-	
-	public function buscar_actividad() {
-		
 		$id = $_REQUEST['id'];
 		
 		check_ajax_referer( 'buscar_actividad_nonce', 'nonce' );
@@ -241,8 +279,8 @@ class ActividadesMuniCordoba {
 		die();
 	}
 	
-	private function mejorar_contenido_actividad($actividad) {
-		
+	private function mejorar_contenido_actividad($actividad)
+	{
 		$actividad['descripcion'] = $this->url_a_link($actividad['descripcion']);
 		$actividad['fecha_actividad'] = $actividad['inicia'] ? $this->formatear_fecha_tres_caracteres($actividad['inicia']) : '';
 		$actividad['fecha_inicia'] = $actividad['inicia'] ? $this->formatear_fecha_inicio_fin($actividad['inicia']) : '';
@@ -251,24 +289,45 @@ class ActividadesMuniCordoba {
 		return $actividad;
 	}
 	
-	private function url_a_link($texto) {
+	/* Funciones de utilidad */
+	
+	private function quitar_palabras($texto, $palabras_devueltas)
+	{
+		$resultado = $texto;
+		$texto = preg_replace('/(?<=\S,)(?=\S)/', ' ', $texto);
+		$texto = str_replace("\n", " ", $texto);
+		$arreglo = explode(" ", $texto);
+		if (count($arreglo) <= $palabras_devueltas) {
+			$resultado = $texto;
+		} else {
+			array_splice($arreglo, $palabras_devueltas);
+			$resultado = implode(" ", $arreglo) . "...";
+		}
+		return $resultado;
+	}
+	
+	private function url_a_link($texto)
+	{
 		$reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
 		return preg_match($reg_exUrl, $texto, $url) ? preg_replace($reg_exUrl, "<a target='_blank' href='" . $url[0] . "'>" . (strlen($url[0]) > 30 ? substr($url[0], 0, 29).'...' : $url[0]) . "</a> ", $texto) : $texto;
 	}
 	
-	private function formatear_fecha_tres_caracteres($timestamp) {
+	private function formatear_fecha_tres_caracteres($timestamp)
+	{
 		$fecha = date_format(date_create($timestamp),"M j");
 		$fecha = $this->traducir_meses($fecha); // Ene 1
 		return $fecha;
 	}
 	
-	private function formatear_fecha_inicio_fin($timestamp) {
+	private function formatear_fecha_inicio_fin($timestamp)
+	{
 		$fecha = date_format(date_create($timestamp),'j \d\e M\, g:i A');
 		$fecha = $this->traducir_meses($fecha); // 1 de Ene, 7:00 PM
 		return $fecha;
 	}
 	
-	private function traducir_meses($texto) {
+	private function traducir_meses($texto)
+	{
 		return str_ireplace(self::$MONTHS, self::$MESES, $texto);
 	}
 }
--- actividad-publica-municipalidad-cordoba/buscador-actividades-template.php
+++ actividad-publica-municipalidad-cordoba/buscador-actividades-template.php
@@ -18,139 +18,160 @@ $actividades = $_POST['datos']['actividades']['results'];
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-			
-			<h1>Buscador de actividades</h1>
-			
-			<div id="bmc" class="c-buscador">
-				<div class="c-buscador__cuerpo">
-					<div class="c-buscador__contenido">
-						<ul class="c-actividades">
-						<?php foreach($actividades as $key => $a) { ?>
-							<li data-id="<?= $a['id'] ?>" class="o-actividad" data-lugar="<?= $a['lugar']['id'] ?>" data-disciplina="<?= $a['disciplinas_ids'] ?>" data-tipo="<?= $a['tipos_ids'] ?>" data-evento="<?= $a['agrupador']['id'] ?>" >
-								<div class="o-actividad__informacion">
-									<h3 title="<?= $a['titulo'] ?>" class="o-actividad__titulo"><?= $a['nombre_corto'] ?></h3><span class="o-actividad__fecha-actividad"><?= $a['fecha_actividad'] ?></span>
-									<p><?= $a['descripcion'] ?></p>
-								</div>
-								<div class="o-actividad__contenedor-imagen"><img class="o-actividad__imagen" src="<?= $a['imagen_final'] ?>" /></div>
-							</li>
-						<?php } ?>
-						</ul>
-					</div>
-					<img class="c-loading" src="<?=$_POST['URL_PLUGIN']."/images/loading.gif"?>" alt="Cargando..." />
-					<div class="c-actividades--particular">
-						<div data-id="" class="o-actividad o-actividad--particular">
-							<div class="o-actividad--particular__informacion">
-								<h3 title="" class="o-actividad__titulo"></h3><span class="o-actividad__fecha-actividad"></span>
-								<p class="o-actividad__evento"></p>
-								<p class="c-tipos"></p>
-								<p class="o-actividad__descripcion"></p>
-								<p class="o-actividad__lugar"></p>
-								<p class="o-actividad__fecha-inicia"></p>
-								<p class="o-actividad__fecha-termina"></p>
-							</div>
-							<div class="c-social">
-								<span>Compartir: </span> <button class="c-social__boton c-social__boton--twitter">Twitter</button> <button class="c-social__boton c-social__boton--facebook">Facebook</button>
-							</div>
-							<a class="c-atras" href="#">Atrás</a>
-							<div class="o-actividad__contenedor-imagen"><img class="o-actividad__imagen" alt="Evento" src="<?=$_POST['URL_PLUGIN']."/images/evento-predeterminado.png"?>"></div>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<?php
+					while ( have_posts() ) : the_post();
+						global $more;
+						$more = 1;
+				?>
+					<h1 class="page-title"><?=the_title();?></h1>
+					<div <?=body_class();?>>
+				<?php
+						the_content();
+					endwhile;
+				?>
+				</div>
+				<div id="bmc" class="c-buscador">
+					<div class="c-buscador__cuerpo">
+						<div class="c-buscador__contenido">
+							<ul class="c-actividades">
+							<?php foreach($actividades as $key => $a) { ?>
+								<li data-id="<?= $a['id'] ?>" class="o-actividad" data-lugar="<?= $a['lugar']['id'] ?>" data-disciplina="<?= $a['disciplinas_ids'] ?>" data-tipo="<?= $a['tipos_ids'] ?>" data-evento="<?= $a['agrupador']['id'] ?>" >
+									<div class="o-actividad__informacion">
+										<h3 title="<?= $a['titulo'] ?>" class="o-actividad__titulo"><?= $a['nombre_corto'] ?></h3><span class="o-actividad__fecha-actividad"><?= $a['fecha_actividad'] ?></span>
+										<p><?= $a['descripcion'] ?></p>
+									</div>
+									<div class="o-actividad__contenedor-imagen"><img class="o-actividad__imagen" src="<?= $a['imagen_final'] ?>" /></div>
+								</li>
+							<?php } ?>
+							</ul>
 						</div>
+						<img class="c-loading" src="<?=$_POST['URL_PLUGIN']."/images/loading.gif"?>" alt="Cargando..." />
+						<div class="c-actividades--particular">
+							<div data-id="" class="o-actividad o-actividad--particular">
+								<div class="o-actividad--particular__informacion">
+									<h3 title="" class="o-actividad__titulo"></h3><span class="o-actividad__fecha-actividad"></span>
+									<p class="o-actividad__evento"></p>
+									<p class="c-tipos"></p>
+									<p class="o-actividad__descripcion"></p>
+									<p class="o-actividad__lugar"></p>
+									<p class="o-actividad__fecha-inicia"></p>
+									<p class="o-actividad__fecha-termina"></p>
+								</div>
+								<div class="c-social">
+									<span>Compartir: </span> <button class="c-social__boton c-social__boton--twitter">Twitter</button> <button class="c-social__boton c-social__boton--facebook">Facebook</button>
+								</div>
+								<a class="c-atras" href="#">Atrás</a>
+								<div class="o-actividad__contenedor-imagen"><img class="o-actividad__imagen" alt="Evento" src="<?=$_POST['URL_PLUGIN']."/images/evento-predeterminado.png"?>"></div>
+							</div>
+						</div>
+						<div class="c-mensaje"><p></p><a class="c-atras" href="#">Atrás</a></div>
 					</div>
-					<div class="c-mensaje"><p></p><a class="c-atras" href="#">Atrás</a></div>
-				</div>
-				
-				<div class="l-contenedor-sidebar">
-				<!-- Barra lateral -->
-					<aside class="c-sidebar" role="navigation">
-					<div class="c-sidebar__header">
-						<div class="c-sidebar__barra-superior"></div>
-						<button class="c-sidebar__toggle">
-							<span class="c-cruz c-cruz--fino"></span>
-						</button>
-						<img class="c-sidebar__imagen" src="<?=$_POST['URL_PLUGIN']."/images/logo-horizontal-blanco.png"?>">
-					</div>
-					<ul class="c-sidebar__nav">
-						<li class="c-dropdown">
-							<a href="#" class="c-dropdown__link" data-toggle="dropdown">
-								Categoría<!-- <span class="c-sidebar__badge"></span>-->
-								<b class="c-dropdown__caret"></b>
-							</a>
-							<ul class="c-dropdown__menu">
-								<?php foreach($eventos as $key => $e) { ?>
-								<li class="c-dropdown__item" data-filtro="evento" data-id="<?= $e['id'] ?>">
-									<a class="c-dropdown__link" href="#" tabindex="-1">
-										<?= $e['nombre'] ?>
-									</a>
-								</li>
-								<?php } ?>
-							</ul>
-						</li>
-						<li class="c-dropdown">
-							<a href="#" class="c-dropdown__link" data-toggle="dropdown">
-								Tipo de Actividad
-								<b class="c-dropdown__caret"></b>
-							</a>
-							<ul class="c-dropdown__menu">
-								<?php foreach($tipo_actividad as $key => $ta) { ?>
-								<li class="c-dropdown__item" data-filtro="tipo" data-id="<?= $ta['id'] ?>">
-									<a class="c-dropdown__link" href="#" tabindex="-1">
-										<?= $ta['nombre'] ?>
-									</a>
-								</li>
-								<?php } ?>
-							</ul>
-						</li>
-						<li class="c-dropdown">
-							<a href="#" class="c-dropdown__link" data-toggle="dropdown">
-								Disciplinas
-								<b class="c-dropdown__caret"></b>
-							</a>
-							<ul class="c-dropdown__menu">
-								<?php foreach($disciplinas as $key => $d) { ?>
-								<li class="c-dropdown__item" data-filtro="disciplina" data-id="<?= $d['id'] ?>">
-									<a class="c-dropdown__link" href="#" tabindex="-1">
-										<?= $d['nombre'] ?>
-									</a>
-								</li>
-								<?php } ?>
-							</ul>
-						</li>
-						<li class="c-dropdown">
-							<a href="#" class="c-dropdown__link" data-toggle="dropdown">
-								Lugares
-								<b class="c-dropdown__caret"></b>
-							</a>
-							<ul class="c-dropdown__menu">
-								<?php foreach($lugares as $key => $l) { ?>
-								<li class="c-dropdown__item" data-filtro="lugar" data-id="<?= $l['id'] ?>">
-									<a class="c-dropdown__link" href="#" tabindex="-1">
-										<?= $l['nombre'] ?>
-									</a>
-								</li>
-								<?php } ?>
-							</ul>
-						</li>
-					</aside>
 					
-					<button class="c-hamburger c-hamburger--3dx" tabindex="0" type="button">
-						<span class="c-hamburger__contenido">
-							<span class="c-hamburger__interno"></span>
-						</span>
-					</button>
+					<div class="l-contenedor-sidebar">
+					<!-- Barra lateral -->
+						<aside class="c-sidebar" role="navigation">
+						<div class="c-sidebar__header">
+							<div class="c-sidebar__barra-superior"></div>
+							<button class="c-sidebar__toggle">
+								<span class="c-cruz c-cruz--fino"></span>
+							</button>
+							<img class="c-sidebar__imagen" src="<?=$_POST['URL_PLUGIN']."/images/logo-horizontal-blanco.png"?>">
+						</div>
+						<ul class="c-sidebar__nav">
+							<li class="c-dropdown">
+								<a href="#" class="c-dropdown__link" data-toggle="dropdown">
+									Categoría<!-- <span class="c-sidebar__badge"></span>-->
+									<b class="c-dropdown__caret"></b>
+								</a>
+								<ul class="c-dropdown__menu">
+									<?php foreach($eventos as $key => $e) { ?>
+									<li class="c-dropdown__item" data-filtro="evento" data-id="<?= $e['id'] ?>">
+										<a class="c-dropdown__link" href="#" tabindex="-1">
+											<?= $e['nombre'] ?>
+										</a>
+									</li>
+									<?php } ?>
+								</ul>
+							</li>
+							<li class="c-dropdown">
+								<a href="#" class="c-dropdown__link" data-toggle="dropdown">
+									Tipo de Actividad
+									<b class="c-dropdown__caret"></b>
+								</a>
+								<ul class="c-dropdown__menu">
+									<?php foreach($tipo_actividad as $key => $ta) { ?>
+									<li class="c-dropdown__item" data-filtro="tipo" data-id="<?= $ta['id'] ?>">
+										<a class="c-dropdown__link" href="#" tabindex="-1">
+											<?= $ta['nombre'] ?>
+										</a>
+									</li>
+									<?php } ?>
+								</ul>
+							</li>
+							<li class="c-dropdown">
+								<a href="#" class="c-dropdown__link" data-toggle="dropdown">
+									Disciplinas
+									<b class="c-dropdown__caret"></b>
+								</a>
+								<ul class="c-dropdown__menu">
+									<?php foreach($disciplinas as $key => $d) { ?>
+									<li class="c-dropdown__item" data-filtro="disciplina" data-id="<?= $d['id'] ?>">
+										<a class="c-dropdown__link" href="#" tabindex="-1">
+											<?= $d['nombre'] ?>
+										</a>
+									</li>
+									<?php } ?>
+								</ul>
+							</li>
+							<li class="c-dropdown">
+								<a href="#" class="c-dropdown__link" data-toggle="dropdown">
+									Lugares
+									<b class="c-dropdown__caret"></b>
+								</a>
+								<ul class="c-dropdown__menu">
+									<?php foreach($lugares as $key => $l) { ?>
+									<li class="c-dropdown__item" data-filtro="lugar" data-id="<?= $l['id'] ?>">
+										<a class="c-dropdown__link" href="#" tabindex="-1">
+											<?= $l['nombre'] ?>
+										</a>
+									</li>
+									<?php } ?>
+								</ul>
+							</li>
+						</aside>
+						
+						<button class="c-hamburger c-hamburger--3dx" tabindex="0" type="button">
+							<span class="c-hamburger__contenido">
+								<span class="c-hamburger__interno"></span>
+							</span>
+						</button>
+					</div>
 				</div>
-			</div>
-			<?php
-				$errores = '';
-				foreach ($_POST['datos'] as $key => $d) {
-					$errores .= isset($d['error']) ? '<li class="c-errores__error">' . $d['error'] .'</li>' : '';
-				}
-				if (strlen($errores) > 0) {
-					?>
-					<ul class="c-errores">
-						<?= $errores ?>
-					</ul>
-					<?php
-				}
-			?></div><!-- #content -->
+				<?php
+					$errores = '';
+					foreach ($_POST['datos'] as $key => $d) {
+						$errores .= isset($d['error']) ? '<li class="c-errores__error">' . $d['error'] .'</li>' : '';
+					}
+					if (strlen($errores) > 0) {
+						?>
+						<ul class="c-errores">
+							<?= $errores ?>
+						</ul>
+						<?php
+					}
+					
+					wp_link_pages( array(
+					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
+					'after'       => '</div>',
+					'link_before' => '<span>',
+					'link_after'  => '</span>',
+					'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
+					'separator'   => '<span class="screen-reader-text">, </span>',
+				) );
+			?>
+			</article>
+		</div><!-- #content -->
 	</div><!-- #primary -->
 </div><!-- #main-content -->
 
